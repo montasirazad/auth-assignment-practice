dcb2687e6cc94baa8470348150e4702713dc19b0
montasirazad <montasirazad@gmail.com>
2021-04-17 20:45:52 +0600

    1st

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="App">
+        <h1>Authentication</h1>
+      </div>
+
+
+      <div className='text' >
+        <p1>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo,
+          officiis. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Facilis pariatur expedita sed tenetur delectus natus alias velit.
+            Perferendis, illum nam. Lorem ipsum dolor sit amet.git </p1>
+      </div>
+      
+
+      <div className='App'>
+      <form>
+      <input   type="email" name="" id="" placeholder='Enter your email'/> <br/>
+      <input type="password" name="" id="" placeholder= 'Password'/>  <br/>
+      <button>Click me</button>
+
+      </form>
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
